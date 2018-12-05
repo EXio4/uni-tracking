da1445f11149a4b449796a378eb7b18725bfdfcb
exio4 <me@exio4.xyz>
2018-12-05 20:52:59 -0300

    Updates

---
+++ precache-manifest.54735616e49e214c1cda1b9a1a2902f0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f968000efc7155f2da56",
+    "url": "/uni-tracking/static/css/main.1837fb92.chunk.css"
+  },
+  {
+    "revision": "f968000efc7155f2da56",
+    "url": "/uni-tracking/static/js/main.f968000e.chunk.js"
+  },
+  {
+    "revision": "c771d0f3b7a506aaaf3c",
+    "url": "/uni-tracking/static/js/1.c771d0f3.chunk.js"
+  },
+  {
+    "revision": "2e3339c4d5f5a2e55353",
+    "url": "/uni-tracking/static/js/runtime~main.2e3339c4.js"
+  },
+  {
+    "revision": "52be8076694a5897b7d01dbdf56b1233",
+    "url": "/uni-tracking/index.html"
+  }
+];
